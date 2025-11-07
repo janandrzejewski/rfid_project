5d0775c5a54167dc6ad9381d6e13799dfa7fdf83
Jan Andrzejewski <janandrzejewski@aol.com>
2025-11-07 23:36:39 +0100

    Implement basic card detection and UID reading

---
+++ RFID_READER/RFID_READER.ino
@@ -0,0 +1,42 @@
+#include <SPI.h>
+#include <MFRC522.h>
+
+#define RST_PIN 9
+#define SS_PIN 10
+
+void print_uid(void);
+
+MFRC522 mfrc522(SS_PIN, RST_PIN);
+
+void setup(void) 
+{
+  Serial.begin(9600);
+  SPI.begin();
+  mfrc522.PCD_Init();
+}
+
+void loop(void)
+{
+  if (!mfrc522.PICC_IsNewCardPresent())
+  {
+    return;
+  }
+  if (!mfrc522.PICC_ReadCardSerial())
+  {
+    return;
+  }
+
+  print_uid();
+}
+
+void print_uid(void)
+{
+  int i = 0;
+  int size = mfrc522.uid.size;
+  Serial.print("Card id: ");
+  for (i = 0; i < size; i++)
+  {
+    Serial.print(mfrc522.uid.uidByte[i], HEX);
+  }
+  Serial.print("\n"); 
+}
